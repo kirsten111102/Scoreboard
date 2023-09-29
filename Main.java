2e6859aa3b1e85b171edd48cb11f0705a1b1253d
Kirsten02 <kirsten11112002@gmail.com>
2023-09-29 16:29:52 +0700

    Scoreboard Everyday Bruh

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //TeamsEnter teamsenterframe = new TeamsEnter();
+
+        BO3Design bo3Design = new BO3Design();
+    }
+}
